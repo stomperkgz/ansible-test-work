7479c91ebda5ed803237b8a0f23bfb87ee0d3cdb
Stomper <stomperkgz@gmail.com>
2021-07-30 13:41:54 +0600

    echo environment variables

--- netcat.sh
+++ netcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do (echo -e 'HTTP/1.1 200 OK\r\n' ; echo -e "\t$(printenv)\n") | nc -lp 80; done
+while true; do (echo -e 'HTTP/1.1 200 OK\r\n' ; echo -e "\t$(printenv EDITOR)\n") | nc -lp 80; done
